84393677a9400700ee9e52e30cce675e49ceb779
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-03-06 10:12:57 -0300

    Removed marker

--- src/com/maxprograms/javapm/Messages.java
+++ src/com/maxprograms/javapm/Messages.java
@@ -13,7 +13,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".javapm"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".javapm";
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
